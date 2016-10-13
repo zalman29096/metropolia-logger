0a918cfb49cfd1a5e3aee3137151157a7a91fd8c
Aleksey <zalman29096@gmail.com>
2016-10-13 09:44:41 +0300

    fixed SCWhitelist.json

--- modules/platform/modules/SearchModule.js
+++ modules/platform/modules/SearchModule.js
@@ -22,27 +22,28 @@ class Search{
 		}
 		
 		var searchCase = "(function() {"+
-				"if(this.applicationToken != '%TOKEN%' || this.userId != '%USER_ID%') return false;"+
-				"var count = 0;"+
-				"var complexSearch = %IS_COMPLEX%;"
-				"var simpleSearch = %SIMPLE_SEARCH%;"+
-				"var complexSearch = %COMPLEX_SEARCH%;"+
-				"for (var key in this['message']) {"+
-					""
-					"if (!complexSearch && simpleSearch.indexOf(this.['message'][key]) >= 0) {"+
-						"count++;"+
-					"}"+	
-					
-				"}"+
-				"return count > 0;"+
-			"})"
+ 				"if(this.applicationToken != '%TOKEN%' || this.userId != '%USER_ID%') return false;"+
+ 				"var count = 0;"+
+ 				"for (var key in this['message']) {"+
+ 					"if (typeof this['message'][key] == 'string' && this['message'][key].indexOf('%SEARCH%') >= 0) {"+
+ 						"count++;"+
+ 					"}"+	
+ 					"if (typeof this['message'][key] == 'number' && (this['message'][key] == parseInt('%SEARCH%') || this['message'][key] == parseFloat('%SEARCH%') )) {"+
+ 						"count++;"+
+ 					"}"+
+ 				"}"+
+ 				"return count > 0;"+
+ 			"})"
+			Log.debug(searchCase
+				.replace(/%TOKEN%/, done.requestData.token)
+				.replace(/%USER_ID%/, this.user._id)
+				.replace(/%SEARCH%/g, done.requestData.search));
 		db.Log.find({
 			
 			"$where": eval(
 				searchCase
 				.replace(/%TOKEN%/, done.requestData.token)
 				.replace(/%USER_ID%/, this.user._id)
-				.replace(/%IS_COMPLEX%/, complexSearch.length > 0)
 				.replace(/%SEARCH%/g, done.requestData.search)
 			)
 		}, function(err, logs){
